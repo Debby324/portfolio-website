6dbcb52b7f8c30ef3c8760bc697473f83ccaeb31
Debby324 <deborahadigolo4@gmail.com>
2025-06-10 02:25:41 +0100

    Create Footer.jsx

---
+++ app/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center py-4 bg-gray-100">
+      <p>© 2025 Deborah</p>
+    </div>
+  );
+};
+
+export default Footer;
